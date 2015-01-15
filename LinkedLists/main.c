81d97457500f3c7f8f7bb131a0e57ed0231a02c1
4johnny <4johnny@users.noreply.github.com>
2015-01-15 00:36:57 -0800

    Add ability to remove target node.

--- LinkedLists/main.c
+++ LinkedLists/main.c
@@ -55,24 +55,64 @@ LinkedListNode* searchLL(LinkedListNode* ll, int target) {
 	return searchLL(ll->next, target);
 }
 
+LinkedListNode* appendToLL(LinkedListNode* ll, int data) {
+	if (ll == NULL) return createLLNode(data, NULL);
+	ll->next = appendToLL(ll->next, data);
+	return ll;
+}
+
+LinkedListNode* removeFromLL(LinkedListNode* currLLNode, LinkedListNode* prevLLNode, LinkedListNode* targetLLNode) {
+	if (targetLLNode == NULL) return currLLNode;
+	if (currLLNode == NULL) return prevLLNode;
+	if (currLLNode == targetLLNode) {
+		if (prevLLNode == NULL) {
+			LinkedListNode* next = currLLNode->next;
+			freeLLNode(&currLLNode);
+			return next;
+		}
+		prevLLNode->next = currLLNode->next;
+		freeLLNode(&currLLNode);
+		return prevLLNode;
+	}
+	removeFromLL(currLLNode->next, currLLNode, targetLLNode);
+	return currLLNode;
+}
+
 int main(int argc, const char * argv[]) {
-
-	// Build up LL in reverse order.
-	LinkedListNode* ll = createLLNode(5, NULL);
-	ll = createLLNode(4, ll);
-	ll = createLLNode(3, ll);
-	ll = createLLNode(2, ll);
-	ll = createLLNode(1, ll);
-
-	// Show stuff
+	
+	// Build list.
+	LinkedListNode* ll = NULL;
+	ll = appendToLL(ll, 1);
+	ll = appendToLL(ll, 2);
+	ll = appendToLL(ll, 3);
+	ll = appendToLL(ll, 4);
+	ll = appendToLL(ll, 5);
+	
+	//	// Build up LL in reverse order.
+	//	LinkedListNode* ll = createLLNode(5, NULL);
+	//	ll = createLLNode(4, ll);
+	//	ll = createLLNode(3, ll);
+	//	ll = createLLNode(2, ll);
+	//	ll = createLLNode(1, ll);
+	
+	// Show list and searched item.
 	printLL(ll);
 	printf("\n");
 	LinkedListNode* targetLLNode = searchLL(ll, 2);
 	printLLNode(targetLLNode);
 	printf("\n");
 	
+	// Show list after item removed.
+	ll = removeFromLL(ll, NULL, targetLLNode);
+	//	ll = removeFromLL(ll, NULL, searchLL(ll, 2));
+	//	ll = removeFromLL(ll, NULL, searchLL(ll, 3));
+	//	ll = removeFromLL(ll, NULL, searchLL(ll, 4));
+	//	ll = removeFromLL(ll, NULL, searchLL(ll, 5));
+	printLL(ll);
+	printf("\n");
+	
 	// Free memory.
 	freeLL(ll);
 	
-    return 0;
+	return 0;
 }
